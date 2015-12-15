16023642336eafc84ed4d289102cde639bcb1943
alumne1daw <alumne1daw@localhost>
2015-12-15 17:47:25 +0100

    gfgeff

--- VisitaCases/src/main/java/net/javierjimenez/App.java
+++ VisitaCases/src/main/java/net/javierjimenez/App.java
@@ -1,13 +1,44 @@
 package net.javierjimenez;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class App {
+
+	private static String RUTA_CASAS = "/ruta.txt";
+
+	public static void main(String[] args) {
+		
+		BufferedReader mapa = null;
+
+		try {
+
+			mapa = new BufferedReader (new FileReader(App.class.getResource(RUTA_CASAS).getFile()));
+
+			String rutaMapa = mapa.readLine();
+			
+			rutaCasas RC = new rutaCasas(rutaMapa);
+
+			int totalCases = RC.rutaMapa();
+
+			System.out.println("Total cases visitades: " + totalCases);
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+
+				if(mapa != null) mapa.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+		}
+	}
 }
---
+++ VisitaCases/src/main/java/net/javierjimenez/rutaCasas.java
@@ -0,0 +1,19 @@
+package net.javierjimenez;
+
+public class rutaCasas {
+
+	private static String ruta;
+	
+	public rutaCasas(String mapa) {
+		
+		ruta = mapa;
+		
+	}
+	
+	public int rutaMapa() {
+		
+		
+		
+		return 0;
+	}
+}
