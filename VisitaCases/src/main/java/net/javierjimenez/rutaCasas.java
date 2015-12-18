65e755dc44d23c62c0a59286e02bcbb0081ce034
alumne1daw <alumne1daw@localhost>
2015-12-18 15:11:49 +0100

    Commit: programa finalizado / falta documentar

--- VisitaCases/src/main/java/net/javierjimenez/rutaCasas.java
+++ VisitaCases/src/main/java/net/javierjimenez/rutaCasas.java
@@ -1,5 +1,8 @@
 package net.javierjimenez;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class rutaCasas {
 
 	private static String ruta;
@@ -12,8 +15,36 @@ public class rutaCasas {
 	
 	public int rutaMapa() {
 		
+		int x = 0; 
+		int y = 0;
 		
+		Set<String> cases = new HashSet<String>();
 		
-		return 0;
+		for (int i = 0; i < ruta.length(); i++) {
+			
+			if(ruta.charAt(i) == '>'){
+				
+				cases.add("Casa (" + x + ", " + y + ")");
+				x++;
+				
+			} else if (ruta.charAt(i) == '<'){
+				
+				cases.add("Casa (" + x + ", " + y + ")");
+				x--;
+				
+			} else if (ruta.charAt(i) == '^'){
+				
+				cases.add("Casa (" + x + ", " + y + ")");
+				y++;
+				
+			} else if (ruta.charAt(i) == 'v'){
+				
+				cases.add("Casa (" + x + ", " + y + ")");
+				y--;
+				
+			}	
+		}
+		
+		return cases.size();
 	}
 }
